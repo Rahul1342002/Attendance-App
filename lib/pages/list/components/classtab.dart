1f8f621753201e2bc3b0fb10961c1fb3946ae1dd
Rahul1342002 <trahul13402@gmail.com>
2023-07-24 12:08:19 +0530

    removed wrap and center

--- lib/pages/list/components/classtab.dart
+++ lib/pages/list/components/classtab.dart
@@ -28,74 +28,72 @@ class _ClassTabState extends State<ClassTab> {
     // Calculate the number of rows needed based on the number of containers and items per row (5 in this case)
     int numberOfRows = (widget.numberOfContainers / 5).ceil();
 
-    return Center(
-      child: Wrap(
-        children: List.generate(
-          numberOfRows,
-          (rowIndex) {
-            int startIndex = rowIndex * 5;
-            int endIndex = (startIndex + 5) > widget.numberOfContainers
-                ? widget.numberOfContainers
-                : (startIndex + 5);
-            return Row(
-              children: List.generate(
-                endIndex - startIndex,
-                (index) {
-                  int rollNumberIndex = startIndex + index;
-                  if (rollNumberIndex >= widget.numberOfContainers) {
-                    return SizedBox
-                        .shrink(); // Use an empty SizedBox for remaining cells
-                  }
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isBoxTappedList[rollNumberIndex] =
-                            !isBoxTappedList[rollNumberIndex];
-                      });
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 65,
-                        height: 65,
-                        decoration: ShapeDecoration(
-                          color: isBoxTappedList[rollNumberIndex]
-                              ? Color(0xFF21D2FF)
-                              : Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          shadows: [
-                            BoxShadow(
-                              color: Color(0x3F000000),
-                              blurRadius: 4,
-                              offset: Offset(0, 4),
-                              spreadRadius: 0,
-                            )
-                          ],
+    return Column(
+      children: List.generate(
+        numberOfRows,
+        (rowIndex) {
+          int startIndex = rowIndex * 5;
+          int endIndex = (startIndex + 5) > widget.numberOfContainers
+              ? widget.numberOfContainers
+              : (startIndex + 5);
+          return Row(
+            children: List.generate(
+              endIndex - startIndex,
+              (index) {
+                int rollNumberIndex = startIndex + index;
+                if (rollNumberIndex >= widget.numberOfContainers) {
+                  return SizedBox
+                      .shrink(); // Use an empty SizedBox for remaining cells
+                }
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isBoxTappedList[rollNumberIndex] =
+                          !isBoxTappedList[rollNumberIndex];
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 65,
+                      height: 65,
+                      decoration: ShapeDecoration(
+                        color: isBoxTappedList[rollNumberIndex]
+                            ? Color(0xFF21D2FF)
+                            : Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        child: Center(
-                          child: Text(
-                            rollNumbers[rollNumberIndex],
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: isBoxTappedList[rollNumberIndex]
-                                  ? Colors.white
-                                  : Color(0xFF568EF8), // Change the text color
-                              fontSize: 30,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w600,
-                            ),
+                        shadows: [
+                          BoxShadow(
+                            color: Color(0x3F000000),
+                            blurRadius: 4,
+                            offset: Offset(0, 4),
+                            spreadRadius: 0,
+                          )
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          rollNumbers[rollNumberIndex],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: isBoxTappedList[rollNumberIndex]
+                                ? Colors.white
+                                : Color(0xFF568EF8), // Change the text color
+                            fontSize: 30,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
                       ),
                     ),
-                  );
-                },
-              ),
-            );
-          },
-        ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
